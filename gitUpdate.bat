f94d5a9c072ea5613db9286c21ae790aa83da949
al0neme <al0neme@tutamail.com>
2026-07-30 23:31:19 +0800

    update

--- gitUpdate.bat
+++ gitUpdate.bat
@@ -2,9 +2,6 @@
 cd %CD%
 echo currentDirectory: %cd%
 
-::复制搜索的html文章到博客Html目录
-copy C:\syn-notes\Wiki\html文章\* C:\al0neme.github.io\Html文章\
-
 ::更新pagefind html索引
 rmdir /s /q pagefind
 pagefind_extended.exe -s . --output-path pagefind
